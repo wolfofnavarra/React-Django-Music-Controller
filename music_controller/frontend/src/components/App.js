a46a3a798764f253fa374d827bff1de2e9cf7262
wolfofnavarra <jfmocnavarro@gmail.com>
2024-10-08 11:30:03 -0600

    need to find fix for the front end not rendering

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -3,18 +3,18 @@ import { render } from "react-dom";
 import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return(
-            <div>
-                <HomePage />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
-const appDiv = document.getElementById("app")
+const appDiv = document.getElementById("app");
 render(<App />, appDiv);
